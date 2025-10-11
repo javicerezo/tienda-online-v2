f108f3c6c3af44f6dfdc449fd557aa7c6eb608c9
Javi Cerezo <jc.webmob@gmail.com>
2025-10-11 11:56:59 +0200

    modificar rutas de los componentes al meterlo todo dentro de la carpeta Ecommerce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Header } from "@/components/layout/Ecommerce/Header/Header";
-import { Top } from "@/components/layout/Top/Top";
-import { Products } from "@/components/layout/Products/Products";
-import { Advantages } from "@/components/layout/Advantages/Advantages";
-import { ProductsVisited } from "@/components/layout/ProductsVisited/ProductsVisited";
-import { Newsletter } from "@/components/layout/Newsletter/Newsletter";
+import { Top } from "@/components/layout/Ecommerce/Top/Top";
+import { Products } from "@/components/layout/Ecommerce/Products/Products";
+import { Advantages } from "@/components/layout/Ecommerce/Advantages/Advantages";
+import { ProductsVisited } from "@/components/layout/Ecommerce/ProductsVisited/ProductsVisited";
+import { Newsletter } from "@/components/layout/Ecommerce/Newsletter/Newsletter";
 
 export default function Home() {
   return (
