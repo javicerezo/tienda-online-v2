5e4c63e82a1cc77d0fe7ff2cbf1fa9936e3aaf02
Javi Cerezo <jc.webmob@gmail.com>
2025-10-11 15:34:00 +0200

    Creación de la base de datos como fichero .ts (para pruebas mientras no tengo el firebase hecho)

---
+++ src/bd/baseDatos.ts
@@ -0,0 +1,293 @@
+import type { productProps } from "@/utils/types/product";
+
+// materialDeportivo es la base de datos
+export const materialDeportivo: productProps[] = [
+    {
+        marca: 'Roxi',
+        nombre: 'Liberty Hoodie W',
+        tipo: 'sudadera',
+        imagen: '/assets/imgs/material/material1.jpg',
+        precio: 115.49,
+        descuento: 0,
+        id: 1
+    },
+    {
+        marca: 'Grifone',
+        nombre: 'Costoia Pants',
+        tipo: 'pantalon',
+        imagen: '/assets/imgs/material/material2.jpg',
+        precio: 50.55,
+        descuento: 18,
+        id: 2
+    },
+    {
+        marca: 'Trangoworld',
+        nombre: 'Velez',
+        tipo: 'sudadera',
+        imagen: '/assets/imgs/material/material3.jpg',
+        precio: 82.80,
+        descuento: 12,
+        id: 3
+    },
+    {
+        marca: 'Grifone',
+        nombre: 'Adur Jacket W',
+        tipo: 'chaqueta impermeable',
+        imagen: '/assets/imgs/material/material4.jpg',
+        precio: 150.15,
+        descuento: 43,
+        id: 4
+    },
+    {
+        marca: 'Salomon',
+        nombre: ' Speed Jaket W',
+        tipo: 'chaqueta plumas plumifero',
+        imagen: '/assets/imgs/material/material5.jpg',
+        precio: 80.45,
+        descuento: 33,
+        id: 5
+    },
+    {
+        marca: 'The North Face',
+        nombre: 'Summit Pro 120',
+        tipo: 'sudadera camiseta',
+        imagen: '/assets/imgs/material/material6.jpg',
+        precio: 322.80,
+        descuento: 50,
+        id: 6
+    },
+    {
+        marca: 'Salomon',
+        nombre: 'Pierra Ment Pant',
+        tipo: 'pantalon',
+        imagen: '/assets/imgs/material/material7.jpg',
+        precio: 150.15,
+        descuento: 60,
+        id: 7
+    },
+    {
+        marca: '+8000',
+        nombre: 'TLT Gtx Overpant',
+        tipo: 'pantalon',
+        imagen: '/assets/imgs/material/material8.jpg',
+        precio: 80.45,
+        descuento: 8,
+        id: 8
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Quark',
+        tipo: 'piolet',
+        imagen: '/assets/imgs/material/material9.jpg',
+        precio: 242.00,
+        descuento: 25,
+        id: 9
+    },
+    {
+        marca: 'Atomic',
+        nombre: 'Nightshade',
+        tipo: 'casco esqui',
+        imagen: '/assets/imgs/material/material10.jpg',
+        precio: 129.99,
+        descuento: 30,
+        id: 10
+    },
+    {
+        marca: 'Scarpa',
+        nombre: 'Ribelle HD',
+        tipo: 'botas alpinismo',
+        imagen: '/assets/imgs/material/material11.jpg',
+        precio: 312.55,
+        descuento: 0,
+        id: 11
+    },
+    {
+        marca: 'La Sportiva',
+        nombre: 'miura',
+        tipo: 'pies de gato escalada',
+        imagen: '/assets/imgs/material/material12.jpg',
+        precio: 145.99,
+        descuento: 9,
+        id: 12
+    },
+    {
+        marca: 'La Sportiva',
+        nombre: 'Kataki',
+        tipo: 'pies de gato escalada',
+        imagen: '/assets/imgs/material/material13.jpg',
+        precio: 151.95,
+        descuento: 9,
+        id: 13
+    },
+    {
+        marca: 'La Sportiva',
+        nombre: 'Katana',
+        tipo: 'pies de gato escalada',
+        imagen: '/assets/imgs/material/material14.jpg',
+        precio: 139.00,
+        descuento: 9,
+        id: 14
+    },
+    {
+        marca: 'Totem Cams',
+        nombre: '0.65',
+        tipo: 'friends escalada clasica',
+        imagen: '/assets/imgs/material/material15.jpg',
+        precio: 84.95,
+        descuento: 0,
+        id: 15
+    },
+    {
+        marca: 'Totem Cams',
+        nombre: '1.80',
+        tipo: 'friends escalada clasica',
+        imagen: '/assets/imgs/material/material16.jpg',
+        precio: 84.95,
+        descuento: 0,
+        id: 16
+    },
+    {
+        marca: 'Totem Cams',
+        nombre: 'Set intermedio',
+        tipo: 'friends escalada clasica',
+        imagen: '/assets/imgs/material/material17.jpg',
+        precio: 264.95,
+        descuento: 0,
+        id: 17
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Tango 8.5mm x 60m',
+        tipo: 'cuerda dinamica escalada',
+        imagen: '/assets/imgs/material/material18.jpg',
+        precio: 140.00,
+        descuento: 29,
+        id: 18
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Paso Guide 7.7mm x 60m',
+        tipo: 'cuerda dinamica escalada',
+        imagen: '/assets/imgs/material/material19.jpg',
+        precio: 152.00,
+        descuento: 25,
+        id: 19
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Volta Guide 9.2mm x 30m',
+        tipo: 'cuerda dinamica escalada',
+        imagen: '/assets/imgs/material/material20.jpg',
+        precio: 116.50,
+        descuento: 29,
+        id: 20
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Pack 6 Djinn Axess 12cm',
+        tipo: 'cintas expres escalada deportiva mosqueton',
+        imagen: '/assets/imgs/material/material21.jpg',
+        precio: 151.95,
+        descuento: 9,
+        id: 21
+    },
+    {
+        marca: 'Fixe',
+        nombre: 'Lotus rosca',
+        tipo: 'cintas expres escalada deportiva mosqueton',
+        imagen: '/assets/imgs/material/material22.jpg',
+        precio: 11.99,
+        descuento: 12,
+        id: 22
+    },
+    {
+        marca: 'Camp',
+        nombre: 'Nano 22 Rack Pack',
+        tipo: 'cintas expres escalada deportiva mosqueton',
+        imagen: '/assets/imgs/material/material23.jpg',
+        precio: 46.00,
+        descuento: 10,
+        id: 23
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'OK Triact-Lock',
+        tipo: 'cintas expres escalada deportiva mosqueton',
+        imagen: '/assets/imgs/material/material24.jpg',
+        precio: 139.00,
+        descuento: 9,
+        id: 24
+    },
+    {
+        marca: 'Kong',
+        nombre: 'Frog Quickdraw',
+        tipo: 'cintas expres escalada deportiva mosqueton',
+        imagen: '/assets/imgs/material/material25.jpg',
+        precio: 35.48,
+        descuento: 14,
+        id: 25
+    },
+    {
+        marca: 'Black Diamond',
+        nombre: 'ATC-Guide',
+        tipo: 'descensor escalada deportiva',
+        imagen: '/assets/imgs/material/material26.jpg',
+        precio: 39.00,
+        descuento: 12,
+        id: 26
+    },
+    {
+        marca: 'Climbing Technology',
+        nombre: 'Alpine Up',
+        tipo: 'descensor escalada deportiva',
+        imagen: '/assets/imgs/material/material27.jpg',
+        precio: 115.95,
+        descuento: 20,
+        id: 27
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Grigri +',
+        tipo: 'asegurador escalada deportiva',
+        imagen: '/assets/imgs/material/material28.jpg',
+        precio: 121.00,
+        descuento: 30,
+        id: 28
+    },
+    {
+        marca: 'Fixe',
+        nombre: 'Parabold M12 x 90mm (20 uds)',
+        tipo: 'escalada deportiva equipamiento',
+        imagen: '/assets/imgs/material/material29.jpg',
+        precio: 110.45,
+        descuento: 30,
+        id: 29
+    },
+    {
+        marca: 'Grivel',
+        nombre: 'Stealth Titanium',
+        tipo: 'escalada deportiva casco',
+        imagen: '/assets/imgs/material/material30.jpg',
+        precio: 102.00,
+        descuento: 23,
+        id: 30
+    },
+    {
+        marca: 'Mammut',
+        nombre: 'Skywalker 2',
+        tipo: 'escalada deportiva casco',
+        imagen: '/assets/imgs/material/material31.jpg',
+        precio: 70.00,
+        descuento: 40,
+        id: 31
+    },
+    {
+        marca: 'Petzl',
+        nombre: 'Meteor S/M',
+        tipo: 'escalada deportiva casco',
+        imagen: '/assets/imgs/material/material32.jpg',
+        precio: 90.75,
+        descuento: 20,
+        id: 32
+    }
+];
