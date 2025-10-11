91cbe0204ca791b41a295384aa0d64b55d876e30
Javi Cerezo <jc.webmob@gmail.com>
2025-10-11 15:34:38 +0200

    arraglo en el section del componente top

--- src/components/layout/Ecommerce/Top/Top.tsx
+++ src/components/layout/Ecommerce/Top/Top.tsx
@@ -5,8 +5,6 @@ export const Top = () => {
     return (
         <section className="Portada">
             <div className="Portada-foto">
-            </div> 
-            <div className="Portada-cesta c-cesta js-cesta">
             </div>
             <div className='Portada-boton'>
                 <Button title={"ver ofertas"} enlace='Products'/>
