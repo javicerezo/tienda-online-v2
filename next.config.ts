aa06e58f634f8c45b635a309d3ee863f98a84a6e
Javi Cerezo <jc.webmob@gmail.com>
2025-10-11 15:33:17 +0200

    Vuelta a la configuración inicial del next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "src/styles")
-    };
-    return config;
-  }
 };
 
 export default nextConfig;
