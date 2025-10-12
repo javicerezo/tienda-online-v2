081934d980db52c241b6f55f2a64c9e3bc558680
Javi Cerezo <jc.webmob@gmail.com>
2025-10-12 12:17:35 +0200

    Creación del componente ProductVisited

--- src/components/layout/Ecommerce/ProductsVisited/ProductsVisited.tsx
+++ src/components/layout/Ecommerce/ProductsVisited/ProductsVisited.tsx
@@ -1,9 +1,54 @@
+'use client'
+
+import { useState } from 'react';
+
+import { ProductCard } from '../Products/ProductCard';
+import { ProductModal } from '../Products/ProductModal';
+import type { product } from '@/utils/types/product';
+
 import './ProductsVisited.scss';
 
 export const ProductsVisited = () => {
+    const [ visitedList, setVisitedList ] = useState<product[] | null>([]);
+    const [ cartList, setCartList ] = useState<product[]>([]);
+    const [ modal, setModal ] = useState<product | null>(null);
+
+    const stop = () => {
+        console.log("NO HAGAS NADA CON ESTE CLIC")
+    }
+
     return (
         <section className="ProductsVisited">
+            { visitedList?.length !== 0 && (
+                <div className="ProductsVisited-contenedor">
+                    <h2 className="ProductsVisited-h2">Últimos productos que has visitado</h2>
+                    <ul className="ProductsVisited-grid">
+                        { visitedList?.map( element => (
+                                <ProductCard 
+                                    key={element.id}
+                                    marca={element.marca}
+                                    nombre={element.nombre}
+                                    imagen={element.imagen}
+                                    precio={element.precio}
+                                    descuento={element.descuento}
+                                    id={element.id}
+                                    handleModal={ () => setModal(element) }
+                                    handleCart={ () => setCartList([element]) }
+                                    onclick={() => stop() }
+                                />
+                            ))
+                        }    
+                    </ul>
+                </div>   
+            )}
             
+            { modal && (
+                <ProductModal 
+                    product={modal}
+                    onClose={ () => setModal(null) }
+                    handleCart={ () => setCartList([modal]) }
+                />     
+            )}
         </section>
     );
 }
