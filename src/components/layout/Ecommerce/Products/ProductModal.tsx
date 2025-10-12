774fff04010576a8286f48b170926acf2bf8d372
Javi Cerezo <jc.webmob@gmail.com>
2025-10-12 08:59:31 +0200

    Creación del componente ProductModal

---
+++ src/components/layout/Ecommerce/Products/ProductModal.tsx
@@ -0,0 +1,8 @@
+import { productModalProps } from "@/utils/types/product"
+
+
+export const ProductModal = ( {product, isOpen, onClose }: productModalProps)  => {
+    return 
+        // <p>hola</p>
+    
+}
