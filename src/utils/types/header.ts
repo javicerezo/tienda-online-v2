bb6e7cc7487a187efc625054d58f9b2e8161f849
Javi Cerezo <jc.webmob@gmail.com>
2025-10-13 13:34:18 +0200

    modificación del type para funcionalidad anterior

--- src/utils/types/header.ts
+++ src/utils/types/header.ts
@@ -2,12 +2,15 @@ import { productCart } from "./product";
 
 export interface headerProps {
     cart: productCart[];
+    eliminateToCart: (number: number) => void;
 }
 
 export interface menuProps {
     cart: productCart[];
+    eliminateToCart: (number: number) => void;
 }
 
 export interface cartCardProps {
     product: productCart;
+    eliminateToCart: (number: number) => void;
 }
