da4b54e0961c9c5e97cbfa62916bfe76b44eb43b
Javi Cerezo <jc.webmob@gmail.com>
2025-10-15 09:43:37 +0200

    Arreglar desfase en el efecto de despliegue del buscador

--- src/components/layout/Ecommerce/Seeker/Seeker.tsx
+++ src/components/layout/Ecommerce/Seeker/Seeker.tsx
@@ -12,7 +12,7 @@ export const Seeker = ({onClose, showSeeker}: seekerProps) => {
     useEffect( () => {
         setTimeout(() => {
             if(seekerContainerRef.current) seekerContainerRef.current.classList.add("Seeker--show");
-        }, 300);
+        }, 100);
     }, [showSeeker]);
 
     const handleClose = () => {
@@ -20,49 +20,47 @@ export const Seeker = ({onClose, showSeeker}: seekerProps) => {
         
         setTimeout( () => {
             onClose();
-        }, 300);
+        }, 500);
     }
 
     return (
         <section className="Seeker" ref={seekerContainerRef}>
-            <div className="Seeker-screen">
-                <div className='Seeker-contenedor'>
-                    <div className='Seeker-cabecera'>
-                        <div className='Seeker-logo'>
-                            <a href="https://javicerezo.github.io/tienda-online/">
-                                <Image 
-                                    width='140' height='60' 
-                                    src="/assets/imgs/logo.svg" 
-                                    alt="logo tienda deportes montaña" 
-                                    loading="lazy" 
-                                />
-                            </a>
-                        </div>
-                        <div className="Seeker-form">
-                            <input className="Seeker-input" id="buscador" type="text" placeholder="Buscar..." />
-                            <FaSearch />
-                        </div>
-                        <button className="Seeker-close" onClick={handleClose}>✕</button>
+            <div className='Seeker-contenedor'>
+                <div className='Seeker-cabecera'>
+                    <div className='Seeker-logo'>
+                        <a href="https://javicerezo.github.io/tienda-online/">
+                            <Image 
+                                width='140' height='60' 
+                                src="/assets/imgs/logo.svg" 
+                                alt="logo tienda deportes montaña" 
+                                loading="lazy" 
+                            />
+                        </a>
                     </div>
-                    <div className='Seeker-contenido'>
-                        <div className='Seeker-productos-populares'>
-                            <p className='Seeker-p'>Búsquedas populares</p>
-                            <ul className='Seeker-ul-populares'>
-                                <li className='Seeker-li-populares'>Pies de gato</li>
-                                <li className='Seeker-li-populares'>Grifone</li>
-                                <li className='Seeker-li-populares'>Sacos de dormir</li>
-                                <li className='Seeker-li-populares'>Petzl</li>
-                                <li className='Seeker-li-populares'>La Sportiva</li>
-                                <li className='Seeker-li-populares'>Cuerdas</li>
-                                <li className='Seeker-li-populares'>Patagonia</li>
-                            </ul>
-                        </div>
-                        <div className='Seeker-productos-buscar'>
-                            <p className='Seeker-p'>Productos recomendados</p>
-                            <ul className='Seeker-ul-buscar js-buscador-ul-buscar'>
-                                {/* {   INSERTAR DINÁMICAMENTE LOS PRODUCTOS QUE COINCIDEN CON EL BUSCADOR      } */}
-                            </ul>
-                        </div>
+                    <div className="Seeker-form">
+                        <input className="Seeker-input" id="buscador" type="text" placeholder="Buscar..." />
+                        <FaSearch />
+                    </div>
+                    <button className="Seeker-close" onClick={handleClose}>✕</button>
+                </div>
+                <div className='Seeker-contenido'>
+                    <div className='Seeker-productos-populares'>
+                        <p className='Seeker-p'>Búsquedas populares</p>
+                        <ul className='Seeker-ul-populares'>
+                            <li className='Seeker-li-populares'>Pies de gato</li>
+                            <li className='Seeker-li-populares'>Grifone</li>
+                            <li className='Seeker-li-populares'>Sacos de dormir</li>
+                            <li className='Seeker-li-populares'>Petzl</li>
+                            <li className='Seeker-li-populares'>La Sportiva</li>
+                            <li className='Seeker-li-populares'>Cuerdas</li>
+                            <li className='Seeker-li-populares'>Patagonia</li>
+                        </ul>
+                    </div>
+                    <div className='Seeker-productos-buscar'>
+                        <p className='Seeker-p'>Productos recomendados</p>
+                        <ul className='Seeker-ul-buscar js-buscador-ul-buscar'>
+                            {/* {   INSERTAR DINÁMICAMENTE LOS PRODUCTOS QUE COINCIDEN CON EL BUSCADOR      } */}
+                        </ul>
                     </div>
                 </div>
             </div>
