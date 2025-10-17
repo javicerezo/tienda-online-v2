9d9a298172cf559650e4cabc60d99a77676df39a
Javi Cerezo <jc.webmob@gmail.com>
2025-10-17 21:56:42 +0200

    creación ficheros para signUp

---
+++ src/app/login/signup/layout.tsx
@@ -0,0 +1,11 @@
+
+export default function Layout (
+    { children }: { children: React.ReactNode }
+) {
+    return (
+        <section> 
+            esto es el layout de sign up
+            {children}
+        </section>
+    );
+}
---
+++ src/app/login/signup/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+
+    return (
+        <>
+            <p>Esto es el sign up</p>
+        </>
+    )
+}
