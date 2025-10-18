2bc17535d3168cf3f49cb78a206d5ffea9c952ce
Javi Cerezo <jc.webmob@gmail.com>
2025-10-18 16:50:19 +0200

    quitar código innecesario

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -15,14 +15,6 @@ const firebaseConfig = {
   messagingSenderId: "396316680011",
   appId: "1:396316680011:web:e4aceb2969d5abe50212ce"
 };
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
