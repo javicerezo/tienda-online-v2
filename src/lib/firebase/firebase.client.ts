164d294dfc2fad741e8bcad8ce05807c8304ce09
Javi Cerezo <jc.webmob@gmail.com>
2025-10-20 13:41:25 +0200

    quitar las credenciales de fichero firebase.client y pasar a fichero .env

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -6,14 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFlKX-6vv5K3masgIi-avj0JzEDQd91HQ",
-  authDomain: "tienda-online-v2-894b2.firebaseapp.com",
-  projectId: "tienda-online-v2-894b2",
-  storageBucket: "tienda-online-v2-894b2.firebasestorage.app",
-  messagingSenderId: "396316680011",
-  appId: "1:396316680011:web:e4aceb2969d5abe50212ce"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROYECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGIN_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
