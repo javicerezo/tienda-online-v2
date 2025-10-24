9dab6d84a260c15e4fa182b369f777a641664c3f
Javi Cerezo <jc.webmob@gmail.com>
2025-10-24 14:38:00 +0200

    crear componente que muestre estado exitoso de la compra y mando a llamar al backend para comprobar de nuevo el pago y agregar la compra a la base de datos

--- src/components/layout/Checkout/SuccessCheckout/SuccessCheckout.tsx
+++ src/components/layout/Checkout/SuccessCheckout/SuccessCheckout.tsx
@@ -2,10 +2,12 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useCart } from '@/utils/hooks/useCart';
+import { auth } from "@/lib/firebase/firebase.client";
+import { onIdTokenChanged } from "firebase/auth";
 
 import './SuccessCheckout.scss';
 import '@/components/ui/Button/Button.scss';
+import { Paragraph } from '@/components/ui/Paragraph/Paragraph';
 
 type LineItem = {
     description: string;
@@ -25,56 +27,99 @@ type SessionSummary = {
 function centsToEUR(n: number) {
     return (n / 100).toFixed(2).replace('.', ',') + ' €';
 }
-
+/**
+ * Limpia el carrito de compra del storage
+ */
+const clearCart = () => {
+    try {
+        localStorage.removeItem('cart'); 
+    } catch {}
+};
 export const SuccessCheckout = () => {
     const params = useSearchParams();
     const router = useRouter();
     const sessionId = params.get('session_id');
 
     const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState<string | null>(null);
     const [data, setData] = useState<SessionSummary | null>(null);
     const [error, setError] = useState<string | null>(null);
-
-    const { clearCart } = useCart();
     
+    // HEMOS VUELTO SE LA SESIÓN DE STRIPE
+    // PRIMERO ESPERAMOS A QUE SE RECUPERE EL USER QUE HAY GUARDADO EN LA SESION PERSISTENTE (VER EN LOGIN)
+    useEffect(() => {
+        const unsub = onIdTokenChanged(auth, async (user) => {
+            if (!user) {
+                setToken(null);
+                return;
+            }
+            const t = await user.getIdToken(true); // fuerza refresh
+            setToken(t);
+        });
+        return () => unsub();
+    }, []);
+
 
     useEffect(() => {
-        const run = async () => {
         if (!sessionId) {
             setError('Falta session_id');
             setLoading(false);
             return;
         }
 
-        try {
-            const req = await fetch(`/.netlify/functions/retrieve-checkout-session?session_id=${encodeURIComponent(sessionId)}`, {
-                method: 'GET',
-                headers: { 'Content-Type': 'application/json' },
-            });
+        if(!token) return
 
-            const res = await req.json();
+        const run = async () => {
+            try {
+                const request = await fetch(`/.netlify/functions/retrieve-checkout-session?session_id=${encodeURIComponent(sessionId)}`, {
+                    method: 'GET',
+                    headers: { 
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${token}`
+                    },
+                });
 
-            if (!req.ok) {
-                setError(res?.message || 'No se pudo verificar el pago');
+                const res = await request.json();
+                if (!request.ok) {
+                    setError(res?.message || 'No se pudo verificar el pago');
+                    setLoading(false);
+                    return
+                }
+
+                setData(res);
+
+                // GUARDAMOS EL PEDIDO EN FIREBASE
+                const request2 = await fetch(`/.netlify/functions/save-order`, {
+                    method: 'POST',
+                    headers: { 
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${token}`
+                    },
+                    body: JSON.stringify({ session_id: sessionId })
+                });
+                
+                const res2 = await request2.json();
+                if(!request2.ok) {
+                    setError(res2?.message || 'No se ha podido guardar el pedido en la base de datos');
+                    setLoading(false);
+                    return;
+                } 
+                
                 setLoading(false);
-                return;
+                
+                // lIMPIAMOS EL CARRITO DE COMPRA
+                if (res.payment_status === 'paid') clearCart();
+
+            } catch {
+                setError('Error inesperado verificando el pago');
             }
-
-            setData(res);
-
-            // Limpia el carrito solo si el pago quedó realizado
-            if (res.payment_status === 'paid') clearCart();
-            setLoading(false);
-        } catch {
-            setError('Error inesperado verificando el pago');
-        }
-    };
+        };
 
         run();
-    }, [ sessionId, clearCart ]);
+    }, [sessionId, token]);
 
-    if (loading) return <p style={{ padding: 16 }}>Comprobando tu pago…</p>;
-    if (error) return <p style={{ padding: 16, color: 'crimson' }}>{error}</p>;
+    if (loading) return <Paragraph text='Comprobando tu pago' styleGreen={true}/>
+    if (error) return <Paragraph text={error} styleGreen={false}/>
     if (!data) return null;
 
     return (
@@ -98,7 +143,7 @@ export const SuccessCheckout = () => {
                 </p>
 
                 <button
-                    onClick={() => router.push('/')}
+                    onClick={ () => { router.push('/') } }
                     className="SuccessCheckout-button Button Button--amarillo"
                     >
                         Volver a la tienda
