a3f4bcb62223185cacf9366a063d9ad16f27d931
Javi Cerezo <jc.webmob@gmail.com>
2025-10-25 12:36:16 +0200

    modificar header

--- src/components/layout/Blog/Header/Header.tsx
+++ src/components/layout/Blog/Header/Header.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { FaBars } from 'react-icons/fa';
 
@@ -22,14 +21,6 @@ export const Header = () => {
             <div className="Header-container">
                 <div className="Header-burger" >
                     <FaBars onClick={openMenuBars}/>
-                </div>
-                <div className="Header-img">
-                    <Image 
-                        width='140' height='60' 
-                        src="/assets/imgs/blog/logo.svg" 
-                        alt="logo tienda deportes montaña" 
-                        loading="lazy" 
-                    />
                 </div>
                 <h2 className="Header-h2">Esquí y montaña</h2>
                 <nav className={`Header-nav ${showMenuBars ? "Header-nav--show" : ""}`}>
