fcdc53928ed9ddf5c1a65bfea912f1656df6d3c5
Javi Cerezo <jc.webmob@gmail.com>
2025-11-04 09:30:36 +0100

    asegurar que next no haga prerenderizado

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,6 +1,9 @@
 import { SuccessCheckout } from "@/components/layout/Checkout/SuccessCheckout/SuccessCheckout";
 import { Suspense } from "react";
 
+// Evita que Next intente prerenderizar esta ruta
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
     return (
         <Suspense fallback={ <div>Comprobando tu pago…</div>} >
