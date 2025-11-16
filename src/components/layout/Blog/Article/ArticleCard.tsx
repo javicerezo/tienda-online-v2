48d48a44eda0638ffe689f826f6e8b9bb9f3280f
Javi Cerezo <jc.webmob@gmail.com>
2025-11-16 12:50:06 +0100

    Crear el card de cada artículo a mostrar de la base de datos

---
+++ src/components/layout/Blog/Article/ArticleCard.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import type { articleProps } from '@/utils/types/new';
+
+import './ArticleCard.scss';
+
+export const ArticleCard = ( {element}: articleProps) => {
+    const { categoria, createdAt, desarrollo, titulo, slug, imagen } = element;
+
+    /**
+     * Formatea la fecha para mostrarla 
+     */
+    const dateLabel = createdAt
+    ? createdAt.toLocaleDateString('es-ES', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      })
+    : '';
+
+    return (
+        <li className='ArticleCard-container'>
+            <Link href={`/blog/categories/${categoria}/${slug}`}>
+                <Image 
+                    className='ArticleCard-img'
+                    width={140} height={60}
+                    src={`/assets/imgs/blog/${imagen}`} 
+                    alt="imagen" 
+                    loading="lazy" 
+                /> 
+            </Link>
+            
+            <div>
+                <Link
+                    className='ArticleCard-title'
+                    href={`/blog/categories/&{categoria}/${slug}`}>
+                    <h5>{titulo}</h5>
+                </Link>
+                <div className="ArticleCard-box">
+                    <Link
+                        className='ArticleCard-category'
+                        href={`/blog/categories/${categoria}`}>
+                        {categoria}
+                    </Link>
+                    <Link
+                        className='ArticleCard-date'
+                        href={`/blog/categories/&{categoria}/${slug}`}>
+                        {dateLabel}
+                    </Link>
+                </div>
+                <p className="ArticleCard-text">{desarrollo}</p>
+            </div>
+        </li>
+    )
+}
