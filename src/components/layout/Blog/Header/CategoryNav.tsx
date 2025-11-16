45b144644936fd34516784414b113be83cc47716
Javi Cerezo <jc.webmob@gmail.com>
2025-11-16 14:36:55 +0100

    Maquetar el navegador de categorías con pathName dinámico

--- src/components/layout/Blog/Header/CategoryNav.tsx
+++ src/components/layout/Blog/Header/CategoryNav.tsx
@@ -11,23 +11,23 @@ export const CategoryNav = () => {
         <nav className='CategoryNav'>
             <ul className="CategoryNav-ul">
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/blog" ? "CategoryNav-li--mod" : ""} `}
+                    className={`CategoryNav-li ${pathName === `/blog/categories/rutas` ? "CategoryNav-li--mod" : ""} `}
                     href={`/blog/categories/rutas`}>Rutas</Link>
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
+                    className={`CategoryNav-li ${pathName === `/blog/categories/noticias` ? "CategoryNav-li--mod" : ""} `}
                     href={`/blog/categories/noticias`}>Noticias</Link>
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
+                    className={`CategoryNav-li ${pathName === `/blog/categories/consejos` ? "CategoryNav-li--mod" : ""} `}
                     href={`/blog/categories/consejos`}>Consejos</Link>
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
+                    className={`CategoryNav-li ${pathName === `/blog/categories/reportajes` ? "CategoryNav-li--mod" : ""} `}
                     href={`/blog/categories/reportajes`}>Reportajes</Link>
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
+                    className={`CategoryNav-li ${pathName === `/blog/categories/test` ? "CategoryNav-li--mod" : ""} `}
                     href={`/blog/categories/test`}>Test</Link>
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href={`/blog/categories/preparación-física`}>Preparación Física</Link>
+                    className={`CategoryNav-li ${pathName === `/blog/categories/preparacion-fisica` ? "CategoryNav-li--mod" : ""} `}
+                    href={`/blog/categories/preparacion-fisica`}>Preparación Física</Link>
             </ul>
         </nav>
     )
