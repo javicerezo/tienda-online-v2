e5c60ce6a4c22c5acf2ca63bdc4c06f3ebc6ef60
Javi Cerezo <jc.webmob@gmail.com>
2025-11-16 12:50:33 +0100

    crear el nav de las categorías

--- src/components/layout/Blog/Header/CategoryNav.tsx
+++ src/components/layout/Blog/Header/CategoryNav.tsx
@@ -11,20 +11,23 @@ export const CategoryNav = () => {
         <nav className='CategoryNav'>
             <ul className="CategoryNav-ul">
                 <Link 
-                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href="">Rutas</Link>
+                    className={`CategoryNav-li ${pathName === "/blog" ? "CategoryNav-li--mod" : ""} `}
+                    href={`/blog/categories/rutas`}>Rutas</Link>
                 <Link 
                     className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href="">Noticias</Link>
+                    href={`/blog/categories/noticias`}>Noticias</Link>
                 <Link 
                     className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href="">Consejos</Link>
+                    href={`/blog/categories/consejos`}>Consejos</Link>
                 <Link 
                     className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href="">Reportajes</Link>
+                    href={`/blog/categories/reportajes`}>Reportajes</Link>
                 <Link 
                     className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
-                    href="">Preparación Física</Link>
+                    href={`/blog/categories/test`}>Test</Link>
+                <Link 
+                    className={`CategoryNav-li ${pathName === "/" ? "" : ""} `}
+                    href={`/blog/categories/preparación-física`}>Preparación Física</Link>
             </ul>
         </nav>
     )
